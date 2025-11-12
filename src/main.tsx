b56b0f96ccd64120f28bcfd8c011c9fa2bd0785d
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2025-11-12 17:29:53 -0500

    Change basename for routing properly

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      basename={import.meta.env.PROD ? '/pet-health-tracker-front' : '/'}
+    >
       <App />
     </BrowserRouter>
   </StrictMode>,
