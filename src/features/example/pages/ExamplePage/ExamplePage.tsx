8cfb6c2a0e91e44a1636e360c0c402ef6538f60a
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2025-11-18 15:16:28 -0500

    Create ModalText component for informative and confirmation modals, usage example on /example page

--- src/features/example/pages/ExamplePage/ExamplePage.tsx
+++ src/features/example/pages/ExamplePage/ExamplePage.tsx
@@ -2,9 +2,7 @@ import './ExamplePage.scss';
 import { useState } from 'react';
 import { useAuthStore } from '../../../../store/auth.store';
 import { Button } from '../../../../components/Button/Button';
-import { Example } from '../../components/Example/Example';
 import { useModalStore } from '../../../../store/modal.store';
-import { openConfirmModal } from '../../../../components/Modal/utils/openConfirmModal';
 import { PRIVATE_ROUTES } from '../../../../config/routes';
 import { useNavigate } from 'react-router-dom';
 
@@ -43,103 +41,95 @@ export const ExamplePage = () => {
 
   const handleOpenTestModal = () => {
     openModal({
-      title: 'Example component title',
-      content: <Example />,
+      title: 'Este es el titulo del modal',
+      content: 'Este es el contenido (texto) del modal',
     });
   };
 
   const handleOpenConfirmModal = () => {
-    openConfirmModal({
+    openModal({
       title: '¿Estás seguro?',
-      message: 'Esta acción no se puede deshacer',
+      content: 'Esta acción no se puede deshacer',
+      variant: 'confirm',
       onConfirm: () => {
         console.log('Confirmado');
       },
+      confirmLabel: 'Confirmar',
+      cancelLabel: 'Cancelar',
     });
   };
+
   const navigate = useNavigate();
 
   return (
-    <section className="example-page">
-      <div className="example-page__container">
-        <div className="example-page__section">
-          <h2>{isRegister ? 'Register Test' : 'Login Test'}</h2>
-          {error && <p className="example-page__error">Error: {error}</p>}
-          <form onSubmit={isRegister ? handleRegister : handleLogin}>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <Button type="submit" disabled={loading} fullWidth>
-              {loading ? 'Loading...' : isRegister ? 'Register' : 'Login'}
-            </Button>
-          </form>
-          <div className="example-page__actions">
-            <Button
-              variant="secondary"
-              onClick={() => setIsRegister(!isRegister)}
-              disabled={loading}
-            >
-              Switch to {isRegister ? 'Login' : 'Register'}
-            </Button>
-            {/* {!isRegister && (
-              <>
-                <Button
-                  onClick={handleTestSuccess}
-                  disabled={loading}
-                  size="sm"
-                >
-                  Test Success
-                </Button>
-                <Button onClick={handleTestError} disabled={loading} size="sm">
-                  Test Error
-                </Button>
-              </>
-            )} */}
-            {isAuthenticated ? (
-              <Button onClick={logout} variant="outline" size="sm">
-                Logout
+    <>
+      <section className="example-page">
+        <div className="example-page__container">
+          <div className="example-page__section">
+            <h2>{isRegister ? 'Register Test' : 'Login Test'}</h2>
+            {error && <p className="example-page__error">Error: {error}</p>}
+            <form onSubmit={isRegister ? handleRegister : handleLogin}>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <Button type="submit" disabled={loading} fullWidth>
+                {loading ? 'Loading...' : isRegister ? 'Register' : 'Login'}
               </Button>
-            ) : (
+            </form>
+            <div className="example-page__actions">
               <Button
-                onClick={() => {
-                  mockLogin();
-                  navigate(PRIVATE_ROUTES.DASHBOARD);
-                }}
-                size="md"
+                variant="secondary"
+                onClick={() => setIsRegister(!isRegister)}
+                disabled={loading}
               >
-                Mock Login
+                Switch to {isRegister ? 'Login' : 'Register'}
               </Button>
-            )}
+              {isAuthenticated ? (
+                <Button onClick={logout} variant="outline" size="sm">
+                  Logout
+                </Button>
+              ) : (
+                <Button
+                  onClick={() => {
+                    mockLogin();
+                    navigate(PRIVATE_ROUTES.DASHBOARD);
+                  }}
+                  size="md"
+                >
+                  Mock Login
+                </Button>
+              )}
+            </div>
           </div>
-        </div>
 
-        <div className="example-page__section">
-          <h2>Modal tests</h2>
-          <div className="example-page__modal-actions">
-            <Button onClick={handleOpenTestModal} disabled={loading}>
-              Test Modal
-            </Button>
-            <Button
-              onClick={handleOpenConfirmModal}
-              disabled={loading}
-              variant="secondary"
-            >
-              Test Confirm
-            </Button>
+          <div className="example-page__section">
+            <h2>Modal text tests (Text and Confirmacion)</h2>
+            <div className="example-page__modal-actions">
+              <Button onClick={handleOpenTestModal} disabled={loading}>
+                Modal Text (Informative)
+              </Button>
+              <Button
+                onClick={handleOpenConfirmModal}
+                disabled={loading}
+                variant="secondary"
+              >
+                Confirmation Modal
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
